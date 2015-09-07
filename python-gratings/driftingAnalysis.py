4428b8b59ed584edbe68239f99c50fb6a69b2853
Athul Vijayan <athulvijayan6@gmail.com>
2015-09-07 13:35:36 +0530

    porting complete

--- python-gratings/driftingAnalysis.py
+++ python-gratings/driftingAnalysis.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 # @Author: Athul
 # @Date:   2015-09-04 16:42:24
-# @Last Modified by:   Athul
-# @Last Modified time: 2015-09-05 19:10:09
+# @Last Modified by:   Athul Vijayan
+# @Last Modified time: 2015-09-06 17:55:48
 from __future__ import division
 import numpy as np
 import scipy.io
 import osi
 from scipy.cluster.vq import kmeans, whiten, vq
+import plotly.plotly as py
+from plotly.graph_objs import *
 
 dataTargets = ['dataset/Mouse-A/', 'dataset/Mouse-B/', 'dataset/Mouse-C/', 'dataset/Mouse-D/', 'dataset/Mouse-E/']
-for mouse in xrange(5):
+for mouse in xrange(1):
     data = scipy.io.loadmat('../driftingGratings/'+ dataTargets[mouse] + 'Data.mat')
     data = data['Data']
     rawData = data[0, 0]['rawF']
@@ -25,7 +27,7 @@ for mouse in xrange(5):
     dircirvar = np.zeros((smoothData.shape[0]))
     for i in xrange(smoothData.shape[0]):
         for j in xrange(stimuliSeq.size):
-            cellData[i, j] = np.append(smoothData[i, 120*j: 120*(j+1)], np.radians(stimuliSeq[j]) )
+            cellData[i, j] = np.append(smoothData[i, 120*j: 120*(j+1)], stimuliSeq[j] )
         # sort the array
         cellData[i] = cellData[i, np.argsort(cellData[i, :, -1])]
         # Calculate spike rate response of each neuron as a real number
@@ -51,62 +53,79 @@ for mouse in xrange(5):
         dircirvar[i] = osi.dirCirVar(spikeRate[i])
 
      # % ----------------------- k-means clustering ----------
-    featureVec = np.vstack((OSI, DSI, cirvar, dircirvar))
+    featureVec = np.vstack((cirvar, dircirvar))
     featureVec = np.transpose(featureVec)
-    featureVec = whiten(featureVec)
+    whiteFeatureVec = whiten(featureVec)
     numClusters = 3
-    centroids = kmeans(featureVec, numClusters)[0]
-    idx = vq(featureVec, centroids)[0]
+    centroids = kmeans(whiteFeatureVec, numClusters)[0]
+    idx = vq(whiteFeatureVec, centroids)[0]
 
     # % Analyze the k means clustering results.
-    globalMean = np.mean(featureVec, 0)
+    globalMean = np.mean(whiteFeatureVec, 0)
     total_ss = 0
     for i in xrange(featureVec.shape[0]):
-        total_ss = total_ss + np.linalg.norm(globalMean - featureVec[i, :])
+        total_ss = total_ss + np.linalg.norm(globalMean - whiteFeatureVec[i, :])
     between_ss = 0
     for i in xrange(numClusters):
         between_ss = between_ss + np.bincount(idx)[i]*np.linalg.norm(globalMean - centroids[i, :])
     clusterEfficiency = (between_ss/total_ss)*100
     print('The clustering is done with an efficiency of ' + str(clusterEfficiency))
     print('To see the definition of clustering efficiency, see the documentation')
-    # % ----------------------- -----------------------------
-
-    showPlots = False;
+    
+    showPlots = False
     if showPlots:
         # % ====================== Plots =======================
-        colors = hsv(10);
-        neuronId = 64;
+        neuronId = 63
+
+        # % ----------------------- --------------------------------
+        # Plot k - means results on a 2D subspace of feature space
+        oriData = {0:[], 1:[], 2:[]}
+        dirData = {0:[], 1:[], 2:[]}
+        names = {0:[], 1:[], 2:[]}
+        j = 0
+        for i in idx:
+            oriData[i].append(featureVec[j, 0])
+            dirData[i].append(featureVec[j, 1])
+            names[i].append(str(j))
+            j += 1
+        trace = []
+        for i in range(3):
+            trace.append(Scatter(x = oriData[i], y = dirData[i], mode='markers+text',
+                name='Class '+str(i), marker=Marker(size=12,), text=names[i], textposition='top center'))
+        plt = Data(trace)
+        layout = Layout(title='Classification of simple and complex cells',
+            xaxis=XAxis(title='Orientation index'),
+            yaxis=YAxis(title='Direction index'))
+        fig = Figure(data=plt, layout=layout)
+        plot_url = py.plot(fig, filename='neuron-classification-kmeans')
+        # ----------------------------------------------------------
 
         # % % 1. Plots each time-series data of length 120 with 
         # % % each trial of an experiment with seperate color
-        # % figure;
-        # % hold on;
-        # % experimentNo = 2;
-        # % for j=1:10
-        # %     h = plot(1:120, cellData{1}(10*(experimentNo-1) + j, 1:120));
-        # %     set(h,'Color', colors(j, :));
-        # %     set(h,'LineWidth',2);
-        # % end
-        # % title('Plots of time-series data with each trial in diffenrent color');
-        # % xlabel('time');
-        # % ylabel('smoothed amplitude');
+        theta = 1
+        trace = []
+        for j in xrange(10):
+            trace.append(Scatter(x = list(xrange(1,121)), y = list(cellData[neuronId, 10*theta+j, :-2]), name='trial '+str(trial)))
+        plt = Data(trace)
+        layout = Layout(title='Plots of time-series data with each trial in diffenrent color',
+            xaxis=XAxis(title='time'),
+            yaxis=YAxis(title='smoothed amplitude'))
+        fig = Figure(data=plt, layout=layout)
+        plot_url = py.plot(fig, filename='neural-response-ts-frame')
         # # % --------------------------------------------------------
 
         # % --------------------------3-----------------------------
         # % 3. Plots response vs angle of stimulus pattern.
-        # figure;
-        # for trial=4
-        #     trialData = spikeRate{neuronId}(trial:10:end, :);
-        #     h = plot(trialData(:, 2), trialData(:, 1));
-        #     set(h,'Color', colors(trial-2, :));
-        #     set(h,'Marker', 'o');
-        #     set(h,'LineStyle', '--');
-        #     set(h,'LineWidth', 2);
-        #     hold on;
-        # end
-        # title('Neuron response (spike rate) Vs direction of stimuli');
-        # ylabel('spike rate');
-        # xlabel('angle in radians');
+        trace = []
+        for trial in xrange(10):
+            trialData = spikeRate[neuronId, trial::10, :]
+            trace.append(Scatter(x=list(trialData[:, 1]), y=list(trialData[:, 0]), name='trial '+str(trial)))
+        plt = Data(trace)
+        layout = Layout(title='Neuron response (spike rate) Vs direction of stimuli',
+            xaxis=XAxis(title='angle in radians'),
+            yaxis=YAxis(title='spike rate'))
+        fig = Figure(data=plt, layout=layout)
+        plot_url = py.plot(fig, filename='neural-response-vs-theta')
         # % --------------------------------------------------------
 
 
@@ -114,24 +133,21 @@ for mouse in xrange(5):
         # % 4. Polar plot of Directional selectivity
         # % Take a neuron, there will be 10 plots corresponding to each trial of the experiment
         # % plot each of the 10 plots in a figure to verify the similarity in response
-        # figure;
-        # for trial=1:10
-        #     % Normalize all trial response from 0 to 1
-        #     normData = abs(spikeRate{neuronId}(trial:10:end, 1))/max(abs(spikeRate{neuronId}(trial:10:end, 1)));
-        #     normData(end+1) = normData(1);
-        #     theta = spikeRate{neuronId}(trial:10:end, 2);
-        #     theta(end+1) = theta(1);
-        #     h = polar(theta, normData);
-        #     set(h,'Color',colors(trial, :));
-        #     set(h,'LineStyle','--');
-        #     set(h,'Marker','o');
-        #     set(h,'LineWidth',2);
-        #     hold on;
-        # end
-        # h = compass(dircirvar(neuronId));
-        # set(h,'LineWidth',3);
-        # set(h,'Color', 'r');
-        # title('Polar plot of Directional selectivity');
+        trace = []
+        for trial in xrange(10):
+            # % Normalize all trial response from 0 to 1
+            normData = np.abs(spikeRate[neuronId, trial::10, 0])/np.max(np.abs(spikeRate[neuronId, trial::10, 0]));
+            normData = np.append(normData, normData[0])
+            theta = spikeRate[neuronId, trial::10, 1]
+            theta = np.append(theta, theta[0])
+            trace.append(Scatter(r=list(normData), t=list(theta), name='trial '+str(trial)))
+
+        # draw dir vector
+        plt = Data(trace)
+        layout = Layout(title='Polar plot of Directional selectivity',)
+        fig = Figure(data=plt, layout=layout)
+        plot_url = py.plot(fig, filename='neural-response-dircirvar')
+
         # % --------------------------------------------------------
 
         # % --------------------------5-----------------------------
@@ -142,25 +158,20 @@ for mouse in xrange(5):
         # % There are observations for 16 angles, but there are only 8 orientations.
         # % We can treat them as two different observations. More data !
 
-        # figure;
-        # for trial=1:10
-        #     % Normalize all trial response from 0 to 1
-        #     normData = abs(spikeRate{neuronId}(trial:10:end, 1))/max(abs(spikeRate{neuronId}(trial:10:end, 1)));
-        #     normData(end+1) = normData(1);
-        #     theta = 2*spikeRate{neuronId}(trial:10:end, 2);
-        #     theta(end+1) = theta(1);
-        #     h = polar(theta, normData);
-        #     set(h,'Color',colors(trial, :));
-        #     set(h,'LineStyle','--');
-        #     set(h,'Marker','o');
-        #     set(h,'LineWidth',2);
-        #     hold on;
-        # end
-        # h = compass(cirvar(neuronId));
-        # set(h,'LineWidth',3);
-        # set(h,'Color', 'r');
-        # title('Polar plot of Orientation selectivity');
-        # % --------------------------2-----------------------------
+        trace = []
+        for trial in xrange(10):
+            # % Normalize all trial response from 0 to 1
+            normData = np.abs(spikeRate[neuronId, trial::10, 0])/np.max(np.abs(spikeRate[neuronId, trial::10, 0]));
+            normData = np.append(normData, normData[0])
+            theta = 2*spikeRate[neuronId, trial::10, 1]
+            theta = np.append(theta, theta[0])
+            trace.append(Scatter(r=list(normData), t=list(theta), name='trial '+str(trial)))
 
+        # draw ori vector
+        plt = Data(trace)
+        layout = Layout(title='Polar plot of Orientation selectivity',)
+        fig = Figure(data=plt, layout=layout)
+        plot_url = py.plot(fig, filename='neural-response-oricirvar')
+        
         # % ********************** Plots ************************
 
--- python-gratings/osi.py
+++ python-gratings/osi.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 # @Author: athul
 # @Date:   2015-09-04 15:23:53
-# @Last Modified by:   Athul
-# @Last Modified time: 2015-09-05 16:56:38
+# @Last Modified by:   Athul Vijayan
+# @Last Modified time: 2015-09-06 17:54:47
 import numpy as np
 
-def calculateSpikeRate(conc):
-    algorithm = 1
+def calculateSpikeRate(conc, algo='average'): 
+    '''the function computes the neuron response of a stimulus using the time-series. The function returns a real value which can be thought of as the spike rate during the stimulus period.'''
     conc = np.asarray(conc)
     spikeRate = np.zeros((conc.shape[0], 2))
     for i in xrange(conc.shape[0]):
-        if algorithm == 1:
+        if algo == 'average':
             # This algorithm just computes the average Ca concentration
             # from the time of epoch to end of stimuli.The input expected is smoothed Ca data
             spikeRate[i, 0] = np.mean(conc[i, 80:])
@@ -19,10 +19,12 @@ def calculateSpikeRate(conc):
     return spikeRate
 
 def cirVar(spikeRate):
+    '''Function calculates the L_ori from spike rate response of neuron to each angle and direction of stimuli'''
     cv = np.dot(spikeRate[:, 0],np.exp(2j*spikeRate[:, 1]))/np.sum(spikeRate[:, 0])
     return np.abs(cv)
 
 def dirCirVar(spikeRate):
+    '''Function calculates the L_dir from spike rate response of neuron to each angle and direction of stimuli'''
     dcv = np.dot(spikeRate[:, 0],np.exp(1j*spikeRate[:, 1]))/np.sum(spikeRate[:, 0])
     return np.abs(dcv)
 
