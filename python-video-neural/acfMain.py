e1a0ddbd57628fab5a902ff643fe518aa8ca2305
Athul Vijayan <athulvijayan6@gmail.com>
2016-02-04 10:30:26 +0530

    corrgram made

--- python-video-neural/acfMain.py
+++ python-video-neural/acfMain.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Athul
 # @Date:   2016-02-03 11:36:02
-# @Last Modified by:   Athul
-# @Last Modified time: 2016-02-03 18:21:35
+# @Last Modified by:   athul
+# @Last Modified time: 2016-02-04 09:57:47
 
 from __future__ import division
 import numpy as np
@@ -30,32 +30,36 @@ neuronId = 20
 data = MT_K0
 sample_rate = 20
 ensembleSpikeRate = data[0, vidIndex]
-
-s1 = ensembleSpikeRate[0]
-s2 = ensembleSpikeRate[1]
+n1 , n2 = 0, 1
+s1 = ensembleSpikeRate[n1]
+s2 = ensembleSpikeRate[n2]
 
 # average across trials
 x = np.mean(s1, axis=1)
 target = np.mean(s2, axis=1)
 
-
-if False:
-    width = 50
-    nhat = width
+width = 20
+nhat = width
+if True:
     fig, ax = plt.subplots()
     template = acf.window(x, nhat, width)
     ccfunction, ax = acf.ccf(template, x, ax=ax)
 
-width = 50
-nhat = width
 frame_shift = 10
 while nhat <= target.size:
     template = acf.window(x, nhat, width)
-    a = acf.ccf(template, target)
+    a = acf.ccf(template, x)
     try:
         acfGram = np.vstack((acfGram, a))
     except:
         acfGram = a
     nhat += frame_shift
-
+acfGram = np.transpose(acfGram)
+fig, ax = plt.subplots()
+cax = ax.imshow(acfGram, aspect='auto', origin='lower', interpolation='none')
+ax.grid(True)
+cbar = fig.colorbar(cax, ticks=[-1, 0, 1])
+ax.set_xlabel('time')
+ax.set_ylabel('Correlation function')
+ax.set_title('Correlationgram of neuron '+str(n1)+' and neuron '+str(n2))
 plt.show() 
