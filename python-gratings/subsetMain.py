fb4f69866f88732898c2dba759dc2f16d0554341
Athul Vijayan <athulvijayan6@gmail.com>
2016-02-05 10:09:45 +0530

    pca done

--- python-gratings/subsetMain.py
+++ python-gratings/subsetMain.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 # @Author: Athul
 # @Date:   2016-02-04 14:55:40
-# @Last Modified by:   Athul
-# @Last Modified time: 2016-02-04 18:28:03
+# @Last Modified by:   athul
+# @Last Modified time: 2016-02-05 00:05:28
 from __future__ import division
 import numpy as np
 import scipy.io
 import osi.osi as osi
 import curvefit.fit as fit
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
@@ -39,15 +41,26 @@ for mouse in xrange(1):
         spikeRate[i] = osi.calculateSpikeRate(cellData[i])
         cirvar[i], dircirvar[i], _, _ = osi.computeAll(spikeRate[i])
 
-    fig = plt.figure()
-    ax = fig.add_axes([0.1, 0.2, 0.8, 0.6])
-    for n in xrange(numNeurons):
-        ax.plot(xrange(120), avgData[0, :, n], label='n = '+str(n))
-    ax.set_title('Average response of all neurons to a particular orientation')
-    ax.set_xlabel('time')
-    ax.set_ylabel('Spike rate')
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=12)
-
+    if False:
+        fig = plt.figure()
+        ax = fig.add_axes([0.1, 0.2, 0.8, 0.6])
+        for n in xrange(numNeurons):
+            ax.plot(xrange(120), avgData[0, :, n], label='n = '+str(n))
+        ax.set_title('Average response of all neurons to a particular orientation')
+        ax.set_xlabel('time')
+        ax.set_ylabel('Spike rate')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), ncol=12)
+    for i in xrange(stimuli.size):
+        try:
+            allResponses = np.vstack((allResponses, avgData[i]))
+        except:
+            allResponses = avgData[i]
+    allResponses = preprocessing.scale(allResponses)
+    pca = PCA()
+    pca.fit(allResponses)
+    fig, ax = plt.subplots()
+    varDist = pca.explained_variance_ratio_
+    ax.plot(xrange(len(varDist)), varDist)
     plt.show()
 
 
